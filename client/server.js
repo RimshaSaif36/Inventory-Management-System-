865ddec76405329a03fe09476e7226dcc111e407
RimshaSaif36 <RimshaSaif560@gmail.com>
2026-03-25 13:36:07 +0500

    changes

--- client/server.js
+++ client/server.js
@@ -7,25 +7,25 @@ const port = Number(process.env.PORT || 3000);
 const hostname = '0.0.0.0';
 
 const app = next({
-  dev,
-  dir: __dirname,
-  hostname,
-  port,
+    dev,
+    dir: __dirname,
+    hostname,
+    port,
 });
 
 const handle = app.getRequestHandler();
 
 app
-  .prepare()
-  .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
-    }).listen(port, hostname, () => {
-      console.log(`Next frontend running on ${hostname}:${port}`);
+    .prepare()
+    .then(() => {
+        createServer((req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        }).listen(port, hostname, () => {
+            console.log(`Next frontend running on ${hostname}:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Failed to start Next frontend server:', error);
+        process.exit(1);
     });
-  })
-  .catch((error) => {
-    console.error('Failed to start Next frontend server:', error);
-    process.exit(1);
-  });
